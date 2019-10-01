ca81114998cf607cdbccbdfe18ea7ea02f9b8d88
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2019-10-01 19:51:41 +0200

    Added radamesh-hydro as a submodule Added initial files

---
+++ filename.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Generating standard filenames and sharing them among different modules
+
+VERSION=2019_10_01
+
+
+declare -A ext=(
+  ['chombo']='chombo.h5'
+  ['log']='log'
+)
---
+++ set_me.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Make sure to edit this file before running other scripts
+
+args=( %arg0% %arg1% )
+
+arg_0=${args[0]}
+arg_1=${args[1]}
